0867cd5649d0100025aac9baf4bb2ec856781fa5
SzymonSkoczylas <ss300476@student.polsl.pl>
2024-05-01 18:08:32 +0200

    Initial Commit

---
+++ Pandora/PandoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  PandoraApp.swift
+//  Pandora
+//
+//  Created by achim on 01/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct PandoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
